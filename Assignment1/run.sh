144f60d0cd4ecceac924d097cc508b6853e6482a
shivam <shivamkm@iitk.ac.in>
2021-02-22 13:38:51 +0530

    Makefile added

--- Assignment1/run.sh
+++ Assignment1/run.sh
@@ -1,19 +1,21 @@
 SECONDS=0
 rm output
 touch output
-mpicc halo.c -o halo -lm
+make
 for i in 1 2 3 4 5
+#for i in 1
 do
   for P in 16 36 49 64 
-  #for P in 16 
+#  for P in 16 
   do
     ~/UGP/allocator/src/allocator.out $P 8 > /dev/null
     cat hosts
     for N in 256 1024 4096 16384 65536 262144 1048576
     do
       echo "P "$P "N "$N
-      mpirun -np $P -f hosts ./halo $N 50 >> output
+      mpirun -np $P -f hosts ./exec $N 50 >> output
     done
   done
 done
+make clean
 echo "Total time in seconds "$SECONDS
