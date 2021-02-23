f3004b6d43b755b8795973fed13a7bcf6a52ba54
shivamkm <shivamkm@iitk.ac.in>
2021-02-23 18:42:37 +0530

    run.sh updated

--- Assignment1/run.sh
+++ Assignment1/run.sh
@@ -1,21 +1,16 @@
-SECONDS=0
-rm output
+rm output data
 touch output
+make clean
 make
 for i in 1 2 3 4 5
-#for i in 1
 do
   for P in 16 36 49 64 
-#  for P in 16 
   do
     ~/UGP/allocator/src/allocator.out $P 8 > /dev/null
-    cat hosts
     for N in 256 1024 4096 16384 65536 262144 1048576
     do
-      echo "P "$P "N "$N
       mpirun -np $P -f hosts ./exec $N 50 >> output
     done
   done
 done
-make clean
-echo "Total time in seconds "$SECONDS
+python3 plot.py
