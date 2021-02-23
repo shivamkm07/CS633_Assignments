85647e9f614b090e0190aae06a344e69afe4b0d5
shivam <shivamkm@iitk.ac.in>
2021-02-23 19:29:22 +0530

    Create Hostfile binary added in the repo

--- Assignment1/run.sh
+++ Assignment1/run.sh
@@ -6,7 +6,8 @@ for i in 1 2 3 4 5
 do
   for P in 16 36 49 64 
   do
-    ~/UGP/allocator/src/allocator.out $P 8 > /dev/null
+    ./create_hostfile $P 8 > /dev/null
+    cat hosts
     for N in 256 1024 4096 16384 65536 262144 1048576
     do
       mpirun -np $P -f hosts ./exec $N 50 >> output
