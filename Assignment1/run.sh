560ca00a7c1fd65038db6d5606cafa06c396d5bd
shivam <shivamkm@iitk.ac.in>
2021-02-23 19:35:19 +0530

    Makefiel updated

--- Assignment1/run.sh
+++ Assignment1/run.sh
@@ -1,5 +1,6 @@
-rm output data
+rm -f output data
 touch output
+chmod u+x create_hostfile
 make clean
 make
 for i in 1 2 3 4 5
@@ -7,7 +8,6 @@ do
   for P in 16 36 49 64 
   do
     ./create_hostfile $P 8 > /dev/null
-    cat hosts
     for N in 256 1024 4096 16384 65536 262144 1048576
     do
       mpirun -np $P -f hosts ./exec $N 50 >> output
