d4e4de1e8e9c0022adb114153f8dff401b17bd04
shivamkm <shivamkm@iitk.ac.in>
2021-04-28 18:08:17 +0530

    Plot scipt working

--- Assignment3/plot.py
+++ Assignment3/plot.py
@@ -3,7 +3,6 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 
-sns.set()
 
 Times = []
 with open("collected_data", "r") as file1:
@@ -15,11 +14,12 @@ x_axis = []
 for P in [1,2]:
     for ppn in [1,2,4]:
         x_axis.append("("+str(P)+","+str(ppn)+")")
+print(x_axis)
 
-plt.plot(Times,x_axis,color='red',marker='o')
+plt.plot(x_axis,Times,color='red',marker='o')
 plt.xlabel('(Num_Nodes,PPN)',fontsize=14)
 plt.ylabel('Execution Time',fontsize=14)
-plt.show()
+plt.savefig('plot.jpg')
 
 
 #
